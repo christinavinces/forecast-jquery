ea4c9c9a8f5d7a615657d2d34de3204476a5d632
cvinces <christina.vinces@gmail.com>
2015-08-15 00:37:59 -0400

    update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var main = function() {
-  
+  $('.day').click(function() {
+    $(this).next().toggle(200);  
+    $(this).find('span').toggleClass("glyphicon-minus");     
+  });
 };
 
 $(document).ready(main);
